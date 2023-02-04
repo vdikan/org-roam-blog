3f806f47ad7e3da97db972c5a68255968de08597
Vladimir Dikan <vdikan@vivaldi.net>
2023-02-04 21:51:33 +0100

    prepended package headers

--- org-roam-blog-utils.el
+++ org-roam-blog-utils.el
@@ -1,5 +1,29 @@
 ;;; org-roam-blog-utils.el --- Utility definitions used throughout ORB -*- lexical-binding: t; -*-
 
+;; Copyright (C) 2023  Vladimir Dikan
+
+;; Author: Vladimir Dikan <vdikan@vivaldi.net>
+;; Keywords: extensions, tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
 (require 'org-roam)
 (require 'unidecode)
 (require 'f)
